0fe5e20e026cefa4e5dd940cc4915db5d84b0be2
mlitman <awesomefat@gmail.com>
2024-02-28 14:16:55 -0600

    updated

---
+++ normal class scripts/MySingleton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySingleton
+{
+    public static string currentDirection = "?";
+    private static Room[] theRooms = new Room[100];
+    private static int numRooms = 0;
+    public static Room theCurrentRoom = null;
+    public static Dungeon theDungeon = MySingleton.generateDungeon();
+
+    public static Dungeon generateDungeon()
+    {
+        Room r1 = new Room("R1");
+        Room r2 = new Room("R2");
+        Room r3 = new Room("R3");
+        Room r4 = new Room("R4");
+        Room r5 = new Room("R5");
+        Room r6 = new Room("R6");
+
+        r1.addExit("north", r2);
+        r2.addExit("south", r1);
+        r2.addExit("north", r3);
+        r3.addExit("south", r2);
+        r3.addExit("west", r4);
+        r3.addExit("north", r6);
+        r3.addExit("east", r5);
+        r4.addExit("east", r3);
+        r5.addExit("west", r3);
+        r6.addExit("south", r3);
+
+        Dungeon theDungeon = new Dungeon("the cross");
+        theDungeon.setStartRoom(r1);
+        return theDungeon;
+    }
+
+    public static void addRoom(Room r)
+    {
+        //static context
+        MySingleton.theRooms[numRooms] = r;
+        MySingleton.numRooms++;   
+    }
+    
+}
--- normal class scripts/Room.cs
+++ normal class scripts/Room.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class Room
 {
     private string name;
+
     private string[] theOpenDoors = new string[4];
     private int howManyOpenDoors = 0;
+
+    private Exit[] theExits = new Exit[4];
+    private int howManyExits = 0;
 
     public Room(string name)
     {
         this.name = name;
+    }
+
+    public void addExit(string direction, Room destinationRoom)
+    {
+        if(this.howManyExits < this.theExits.Length)
+        {
+            Exit e = new Exit(direction, destinationRoom);
+            this.theExits[this.howManyExits] = e;
+            this.howManyExits++;
+        }
     }
 
     public void setOpenDoor(string direction)
