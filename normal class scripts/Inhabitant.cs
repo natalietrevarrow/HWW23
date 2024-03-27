76bd92bbb2ad2113dc93799cef31b448593696e2
mlitman <awesomefat@gmail.com>
2024-03-27 11:58:33 -0500

    updated for animations and the start of the fight

--- normal class scripts/Inhabitant.cs
+++ normal class scripts/Inhabitant.cs
@@ -6,10 +6,33 @@ public abstract class Inhabitant
 {
     protected string name;
     protected Room currentRoom;
-
+    protected int hp, ac;
+   
     public Inhabitant(string name)
     {
         this.name = name;
         this.currentRoom = null;
+        this.hp = Random.Range(10, 16);
+        this.ac = Random.Range(8, 17);
+    }
+
+    public Room getCurrentRoom()
+    {
+        return this.currentRoom;
+    }
+
+    public void setCurrentRoom(Room r)
+    {
+        this.currentRoom = r;
+    }
+
+    public int getHP()
+    {
+        return this.hp;
+    }
+
+    public int getAC()
+    {
+        return this.ac;
     }
 }
--- normal class scripts/Player.cs
+++ normal class scripts/Player.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 
 public class Player : Inhabitant
 {
-    private Room currentRoom;
+    
 
     public Player(string name) : base(name)
     {
-        this.name = name;
-        this.currentRoom = null;
     }
 
-    public Room getCurrentRoom()
-    {
-        return this.currentRoom;
-    }
-
-    public void setCurrentRoom(Room r)
-    {
-        this.currentRoom = r;
-    }
+    
 }
